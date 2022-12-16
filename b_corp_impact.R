0dd9f0cb80cc9fe75cf5e1d1a86d5f5861461c97
Travis Horesh <horeshtr@gmail.com>
2022-12-15 21:24:43 -0500

    initial commit

---
+++ b_corp_impact.R
@@ -0,0 +1,28 @@
+#------------------------------------------------------------------#
+#   Project:  B Corp Impact Data
+#   Author:   Travis Horesh
+#   Source:   Data World
+#               https://data.world/blab/b-corp-impact-data
+#   Date:     December 2022
+#------------------------------------------------------------------#
+
+if(!require(shiny)) install.packages("shiny", repos = "http://cran.us.r-project.org")
+if(!require(ggplot2)) install.packages("ggplot2", repos = "http://cran.us.r-project.org")
+if(!require(dplyr)) install.packages("dplyr", repos = "http://cran.us.r-project.org")
+if(!require(lubridate)) install.packages("lubridate", repos = "http://cran.us.r-project.org")
+if(!require(stringr)) install.packages("stringr", repos = "http://cran.us.r-project.org")
+
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(lubridate)
+library(stringr)
+
+data <- read.csv("https://query.data.world/s/qtnt2oj6vfmv43w5kcn4vlc5uf43fa", 
+                 header=TRUE, 
+                 stringsAsFactors=FALSE,
+                 na.strings = c("", " ")
+                )
+glimpse(data)
+head(data)
+
